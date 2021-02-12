c2c8d7cdbadd608b0b936783cfeccd126fa78654
Rossi <admin@rassloff.info>
2021-02-12 20:57:13 +0100

    nochmal

--- login_html.php
+++ login_html.php
@@ -1,8 +1,22 @@
 <?php
 
-require_once("header.inc.php");
+//require_once("header.inc.php");
 
 ?>
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="UTF-8">
+        <title>Arbeitszeitverwaltung</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+    <div>
 <h4>Login with HTML only</h4>
 
 <div class="wrapper fadeInDown">
